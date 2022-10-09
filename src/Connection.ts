9a304ce0d4a6d20b404f2ff3ff594ab7501675ce
Raffaele Preziosi <ra.preziosi@hotmail.com>
2022-10-09 16:22:06 +0200

    Improved Performance

--- src/Connection.ts
+++ src/Connection.ts
@@ -1,129 +1,153 @@
 import * as fs from 'fs';
 import * as path from 'path';
 import * as vscode from 'vscode';
-import { isStandardObject, asyncQuery, getObjectFieldDeveloperName} from './Utils';
+import { isStandardObject, asyncQuery, getObjectFieldDeveloperName } from './Utils';
 import { resetStatusBar } from './StatusBar';
 const execSync = require('child_process').execSync;
 export class Connection {
-  private orgName;
-  private userName;
-  private userId;
-  private debugLevels;
-  private automatedProcessUserId;
-  private platformIntegrationUserId;
-  private instanceUrl;
-  public mapNameClass_MapMethodName_Coverage;
-	public mapNameClass_TotalCoverage;
-  private mapObjectId;
-  private static instance;
+    private orgName;
+    private userName;
+    private userId;
+    private debugLevels;
+    private automatedProcessUserId;
+    private platformIntegrationUserId;
+    private instanceUrl;
+    public mapNameClass_MapMethodName_Coverage;
+    public mapNameClass_TotalCoverage;
+    private mapObjectId;
 
-  private constructor(orgName) {
-    this.orgName = orgName;
-    let response = execSync('sfdx force:auth:list --json');
-    let accessOrgs = JSON.parse(response.toString())["result"];
-    let accessOrg = accessOrgs.find(accessOrg => {
-				if(accessOrg.alias){
-						return accessOrg.alias.split(',').includes(orgName);
-				} return false;
-      }
-    )
-    this.userName = accessOrg.username;
-    this.instanceUrl = accessOrg.instanceUrl;
-    resetStatusBar();
-    this.mapNameClass_MapMethodName_Coverage = new Map();
-    this.mapNameClass_TotalCoverage = new Map();
-  }
+    private static instance;
+    private static accessOrgs;
 
-  public static getConnection() {
-    let connectedOrg = getConnectedOrg();
-    if(!this.instance || (connectedOrg != this.instance.getOrgName())){
-      this.instance = new Connection(connectedOrg);
+    private constructor(orgName) {
+        this.orgName = orgName;
+        let accessOrg = Connection.getAccessOrg(orgName);
+        this.userName = accessOrg["username"];
+        this.instanceUrl = accessOrg["instanceUrl"];
+        resetStatusBar();
+        this.mapNameClass_MapMethodName_Coverage = new Map();
+        this.mapNameClass_TotalCoverage = new Map();
     }
-    return this.instance;
-  }
 
-  public async getUserId(){
-    if(!this.userId){
-      const user = await asyncQuery(`SELECT Id FROM User WHERE Username = '${this.userName}' LIMIT 1`, true);
-      this.userId = user[0].Id
-    }
-    return this.userId;
-  }
-
-  public async getAutomatedProcessUserId(){
-    if(!this.automatedProcessUserId){
-      const user = await asyncQuery(`SELECT Id FROM User WHERE Name = 'Automated Process' LIMIT 1`, true);
-      this.automatedProcessUserId = user[0].Id
-    }
-    return this.automatedProcessUserId;
-  }
-
-  public async getPlatformIntegrationUserId(){
-    if(!this.platformIntegrationUserId){
-      const user = await asyncQuery(`SELECT Id FROM User WHERE Name = 'Platform Integration User' LIMIT 1`, true);
-      this.platformIntegrationUserId = user[0].Id
-    }
-    return this.platformIntegrationUserId;
-  }
-
-  private async populateMapObjectId(){
-    this.mapObjectId = new Map();
-    const customObjects = await asyncQuery(`Select Id,DeveloperName from CustomObject`, true);
-    for (let i = 0; i < customObjects.length; i++) {
-      this.mapObjectId.set(customObjects[i]["DeveloperName"], customObjects[i]["Id"]);
-    }
-  }
-
-  public async getObjectId(objectFolderName){
-    if(isStandardObject(objectFolderName)){
-      return objectFolderName;
-    }else{
-      let objectDeveloperName = getObjectFieldDeveloperName(objectFolderName);
-      if(this.mapObjectId && this.mapObjectId.has(objectDeveloperName)){
-        return this.mapObjectId.get(objectDeveloperName);
-      }else{
-        await this.populateMapObjectId();
-        if(this.mapObjectId.has(objectDeveloperName)){
-          return this.mapObjectId.get(objectDeveloperName);
-        }else{
-          throw 'Object not found in the org!'
+    private static getAccessOrg(orgName) {
+        let accessOrg;
+        if (Connection.accessOrgs) {
+            accessOrg = Connection.findAccessOrg(orgName);
+            if (!accessOrg) {
+                Connection.setAccessOrg();
+                accessOrg = Connection.findAccessOrg(orgName);
+            }
+        } else {
+            Connection.setAccessOrg();
+            accessOrg = Connection.findAccessOrg(orgName);
         }
-      }
+        return accessOrg;
     }
-  }
 
-  public async getDebugLevels(){
-    if(!this.debugLevels){
-      this.debugLevels = await asyncQuery('SELECT Id,DeveloperName FROM Debuglevel', true);
+    private static setAccessOrg() {
+        let response = execSync('sfdx force:auth:list --json');
+        Connection.accessOrgs = JSON.parse(response.toString())["result"];
     }
-    if(this.debugLevels.length > 0){
-      return this.debugLevels;
-    }else{
-      throw 'Retrivered Debug Levels failed';
+
+    private static findAccessOrg(orgName) {
+        return Connection.accessOrgs.find(accessOrg => {
+            if (accessOrg.alias) {
+                return accessOrg.alias.split(',').includes(orgName);
+            }
+            return false;
+        })
     }
-  }
 
-  public getUsername(){
-    return this.userName;
-  }
+    public static getConnection() {
+        let connectedOrg = Connection.getConnectedOrg();
+        if (!this.instance || (connectedOrg != this.instance.getOrgName())) {
+            this.instance = new Connection(connectedOrg);
+        }
+        return this.instance;
+    }
 
-  public getOrgName(){
-    return this.orgName;
-  }
+    private static getConnectedOrg() {
+        try {
+            const sfdxConfigPath = path.join(vscode.workspace.workspaceFolders[0].uri.fsPath, '/.sfdx', 'sfdx-config.json');
+            const bodyConfig = fs.readFileSync(sfdxConfigPath, 'utf-8');
+            const jsonConfig = JSON.parse(bodyConfig);
+            return jsonConfig["defaultusername"];
+        } catch (error) {
+            vscode.window.showInformationMessage('You are not connected to any Salesforce org');
+        }
+    }
 
-  public getInstanceUrl(){
-    return this.instanceUrl;
-  }
-}
+    public async getUserId() {
+        if (!this.userId) {
+            const user = await asyncQuery(`SELECT Id FROM User WHERE Username = '${this.userName}' LIMIT 1`, true);
+            this.userId = user[0].Id
+        }
+        return this.userId;
+    }
 
-function getConnectedOrg() {
-  try{
-    const sfdxConfigPath = path.join(vscode.workspace.workspaceFolders[0].uri.fsPath,'/.sfdx','sfdx-config.json');
-    const bodyConfig = fs.readFileSync(sfdxConfigPath, 'utf-8');
-    const jsonConfig = JSON.parse(bodyConfig);
-    return jsonConfig["defaultusername"];
-  }catch(error){
-    vscode.window.showInformationMessage('You are not connected to any Salesforce org');
-  }
+    public async getAutomatedProcessUserId() {
+        if (!this.automatedProcessUserId) {
+            const user = await asyncQuery(`SELECT Id FROM User WHERE Name = 'Automated Process' LIMIT 1`, true);
+            this.automatedProcessUserId = user[0].Id
+        }
+        return this.automatedProcessUserId;
+    }
+
+    public async getPlatformIntegrationUserId() {
+        if (!this.platformIntegrationUserId) {
+            const user = await asyncQuery(`SELECT Id FROM User WHERE Name = 'Platform Integration User' LIMIT 1`, true);
+            this.platformIntegrationUserId = user[0].Id
+        }
+        return this.platformIntegrationUserId;
+    }
+
+    private async populateMapObjectId() {
+        this.mapObjectId = new Map();
+        const customObjects = await asyncQuery(`Select Id,DeveloperName from CustomObject`, true);
+        for (let i = 0; i < customObjects.length; i++) {
+            this.mapObjectId.set(customObjects[i]["DeveloperName"], customObjects[i]["Id"]);
+        }
+    }
+
+    public async getObjectId(objectFolderName) {
+        if (isStandardObject(objectFolderName)) {
+            return objectFolderName;
+        } else {
+            let objectDeveloperName = getObjectFieldDeveloperName(objectFolderName);
+            if (this.mapObjectId && this.mapObjectId.has(objectDeveloperName)) {
+                return this.mapObjectId.get(objectDeveloperName);
+            } else {
+                await this.populateMapObjectId();
+                if (this.mapObjectId.has(objectDeveloperName)) {
+                    return this.mapObjectId.get(objectDeveloperName);
+                } else {
+                    throw 'Object not found in the org!'
+                }
+            }
+        }
+    }
+
+    public async getDebugLevels() {
+        if (!this.debugLevels) {
+            this.debugLevels = await asyncQuery('SELECT Id,DeveloperName FROM Debuglevel', true);
+        }
+        if (this.debugLevels.length > 0) {
+            return this.debugLevels;
+        } else {
+            throw 'Retrivered Debug Levels failed';
+        }
+    }
+
+    public getUsername() {
+        return this.userName;
+    }
+
+    public getOrgName() {
+        return this.orgName;
+    }
+
+    public getInstanceUrl() {
+        return this.instanceUrl;
+    }
 }
 
--- src/DebugLogHandler.ts
+++ src/DebugLogHandler.ts
@@ -1,122 +1,120 @@
-import {asyncQuery,deleteRecord,createRecord,deleteRecords} from './Utils';
+import { asyncQuery, deleteRecord, createRecord, deleteRecords } from './Utils';
 import { Connection } from './Connection';
-import { showTraceFlagStatus, hideTraceFlagStatus} from './StatusBar';
+import { showTraceFlagStatus, hideTraceFlagStatus } from './StatusBar';
 import * as vscode from 'vscode';
 const LOG_TIMER_LENGTH_MINUTES = 60;
 const MILLISECONDS_PER_MINUTE = 60000;
 const TRACED_ENTITY_TYPES = ['User', 'Automated Process', 'Platform Integration User'];
 
 export async function enableDebugLog() {
-  try{
-    const tracedEntityType = await vscode.window.showQuickPick(TRACED_ENTITY_TYPES, {placeHolder : 'Select Traced Entity Type'})
-    if(tracedEntityType){
-      await vscode.window.withProgress(
-          {
-            title: 'SPB: Get Debug Levels',
-            location: vscode.ProgressLocation.Notification
-          },
-          () => Connection.getConnection().getDebugLevels()
-        );
-
-        let debuglevels = await Connection.getConnection().getDebugLevels();
-
-        const debugLevelSelected = await vscode.window.showQuickPick(debuglevels.map(debuglog => debuglog.DeveloperName),{placeHolder : 'Select Debug Level'})
-        if(debugLevelSelected){
-          await vscode.window.withProgress(
+    try {
+        Promise.all([vscode.window.showQuickPick(TRACED_ENTITY_TYPES, { placeHolder: 'Select Traced Entity Type' }), vscode.window.withProgress(
             {
-              title: 'SPB: Create Trace Flag',
-              location: vscode.ProgressLocation.Notification
+                title: 'SPB: Get Debug Levels',
+                location: vscode.ProgressLocation.Notification
             },
-            () => createTraceFlag(tracedEntityType, debuglevels.find(debuglevel => debuglevel.DeveloperName == debugLevelSelected))
-          );
-        }
+            () => Connection.getConnection().getDebugLevels()
+        )]).then((values) => {
+            let tracedEntityType = values[0];
+            let debuglevels = Array.isArray(values[1]) ? values[1] : [];
+            vscode.window.showQuickPick(debuglevels.map(debuglog => debuglog["DeveloperName"]), { placeHolder: 'Select Debug Level' }).then((debugLevelSelected) => {
+                if (debugLevelSelected) {
+                    vscode.window.withProgress(
+                        {
+                            title: 'SPB: Create Trace Flag',
+                            location: vscode.ProgressLocation.Notification
+                        },
+                        () => createTraceFlag(tracedEntityType, debuglevels.find(debuglevel => debuglevel["DeveloperName"] == debugLevelSelected))
+                    );
+                }
+            })
+        })
+    } catch (error) {
+        console.log(error)
+        vscode.window.showInformationMessage(error);
     }
-  }catch(error){
-    console.log(error)
-    vscode.window.showInformationMessage(error);
-  }
 }
 
-async function createTraceFlag(tracedEntityType, debugLevel){
-  const TracedEntitySfId = await getTraceEntityId(tracedEntityType);
-
-  await deleteActiveTraceFlag(TracedEntitySfId);
-  let expirationDate = new Date(
-    Date.now() + LOG_TIMER_LENGTH_MINUTES * MILLISECONDS_PER_MINUTE
-  );
-
-  const newTraceFlag = {
-    TracedEntityId : TracedEntitySfId,
-    DebugLevelId : debugLevel.Id,
-    StartDate : new Date(Date.now() - 10 * MILLISECONDS_PER_MINUTE).toISOString(),
-    logtype: 'USER_DEBUG',
-    ExpirationDate : expirationDate.toISOString()
-  }
-  await createRecord('TraceFlag',newTraceFlag);
-  showTraceFlagStatus(tracedEntityType, newTraceFlag.ExpirationDate);
-}
-
-async function getTraceEntityId(tracedEntityType){
- let tracedEntityTypeId;
-  if(tracedEntityType === 'User'){
-    tracedEntityTypeId = await Connection.getConnection().getUserId();
-  }else if(tracedEntityType === 'Automated Process'){
-    tracedEntityTypeId = await Connection.getConnection().getAutomatedProcessUserId();
-  }else{
-    tracedEntityTypeId = await Connection.getConnection().getPlatformIntegrationUserId();
-  }
-  return tracedEntityTypeId;
-}
-
-export async function deleteActiveTraceFlag(TracedEntitySfId){
-  const currentTraceFlag = await getActiveTraceFlag(TracedEntitySfId);
-  if(currentTraceFlag){
-    await deleteRecord(currentTraceFlag);
-  }
-}
-
-async function getActiveTraceFlag(TracedEntitySfId){
-  const NOW = new Date(Date.now()).toISOString();
-  const traceFlags = await asyncQuery(`SELECT Id FROM TraceFlag WHERE TracedEntityId = '${TracedEntitySfId}' AND ExpirationDate >= ${NOW} ORDER BY ExpirationDate DESC LIMIT 1`, true);
-  if(traceFlags || traceFlags.length > 0){
-    return traceFlags[0];
-  }else{
-    return null;
-  }
-}
-
-export async function disableDebugLog(){
-
-  const tracedEntityType = await vscode.window.showQuickPick(TRACED_ENTITY_TYPES, {placeHolder : 'Select Traced Entity Type'});
-
-  if(tracedEntityType){
+async function createTraceFlag(tracedEntityType, debugLevel) {
     const TracedEntitySfId = await getTraceEntityId(tracedEntityType);
 
-    await vscode.window.withProgress(
-      {
-        title: 'SPB: Delete Active Trace Flag',
-        location: vscode.ProgressLocation.Notification
-      },
-      () => deleteActiveTraceFlag(TracedEntitySfId)
+    await deleteActiveTraceFlag(TracedEntitySfId);
+    let expirationDate = new Date(
+        Date.now() + LOG_TIMER_LENGTH_MINUTES * MILLISECONDS_PER_MINUTE
     );
-    hideTraceFlagStatus(tracedEntityType);
-  }
+
+    const newTraceFlag = {
+        TracedEntityId: TracedEntitySfId,
+        DebugLevelId: debugLevel.Id,
+        StartDate: new Date(Date.now() - 10 * MILLISECONDS_PER_MINUTE).toISOString(),
+        logtype: 'USER_DEBUG',
+        ExpirationDate: expirationDate.toISOString()
+    }
+    await createRecord('TraceFlag', newTraceFlag);
+    showTraceFlagStatus(tracedEntityType, newTraceFlag.ExpirationDate);
 }
 
-export async function deleteApexLogs(){
-  const userChoise = await vscode.window.showQuickPick(['No','Yes'], {placeHolder : 'Are you sure you want to delete all the Apex Logs from the org?'})
-  if(userChoise === 'Yes'){
-    await vscode.window.withProgress(
-      {
-        title: 'Delete Apex Logs',
-        location: vscode.ProgressLocation.Notification
-      },
-      () => deleteLogs()
-    );
-  }
+async function getTraceEntityId(tracedEntityType) {
+    let tracedEntityTypeId;
+    if (tracedEntityType === 'User') {
+        tracedEntityTypeId = await Connection.getConnection().getUserId();
+    } else if (tracedEntityType === 'Automated Process') {
+        tracedEntityTypeId = await Connection.getConnection().getAutomatedProcessUserId();
+    } else {
+        tracedEntityTypeId = await Connection.getConnection().getPlatformIntegrationUserId();
+    }
+    return tracedEntityTypeId;
 }
 
-async function deleteLogs(){
-  let apexLogs = await asyncQuery(`SELECT Id FROM ApexLog ORDER BY LastModifiedDate ASC LIMIT 50000`, true);
-  await deleteRecords(apexLogs);
+export async function deleteActiveTraceFlag(TracedEntitySfId) {
+    const currentTraceFlag = await getActiveTraceFlag(TracedEntitySfId);
+    if (currentTraceFlag) {
+        await deleteRecord(currentTraceFlag);
+    }
+}
+
+async function getActiveTraceFlag(TracedEntitySfId) {
+    const NOW = new Date(Date.now()).toISOString();
+    const traceFlags = await asyncQuery(`SELECT Id FROM TraceFlag WHERE TracedEntityId = '${TracedEntitySfId}' AND ExpirationDate >= ${NOW} ORDER BY ExpirationDate DESC LIMIT 1`, true);
+    if (traceFlags || traceFlags.length > 0) {
+        return traceFlags[0];
+    } else {
+        return null;
+    }
+}
+
+export async function disableDebugLog() {
+
+    const tracedEntityType = await vscode.window.showQuickPick(TRACED_ENTITY_TYPES, { placeHolder: 'Select Traced Entity Type' });
+
+    if (tracedEntityType) {
+        const TracedEntitySfId = await getTraceEntityId(tracedEntityType);
+
+        await vscode.window.withProgress(
+            {
+                title: 'SPB: Delete Active Trace Flag',
+                location: vscode.ProgressLocation.Notification
+            },
+            () => deleteActiveTraceFlag(TracedEntitySfId)
+        );
+        hideTraceFlagStatus(tracedEntityType);
+    }
+}
+
+export async function deleteApexLogs() {
+    const userChoise = await vscode.window.showQuickPick(['No', 'Yes'], { placeHolder: 'Are you sure you want to delete all the Apex Logs from the org?' })
+    if (userChoise === 'Yes') {
+        await vscode.window.withProgress(
+            {
+                title: 'Delete Apex Logs',
+                location: vscode.ProgressLocation.Notification
+            },
+            () => deleteLogs()
+        );
+    }
+}
+
+async function deleteLogs() {
+    let apexLogs = await asyncQuery(`SELECT Id FROM ApexLog ORDER BY LastModifiedDate ASC LIMIT 50000`, true);
+    await deleteRecords(apexLogs);
 }
